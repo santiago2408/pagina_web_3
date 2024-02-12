6538d89803c68deef0854ffed13aaca28b6f35a2
santiago2408 <95543316+santiago2408@users.noreply.github.com>
2024-02-12 09:57:26 -0500

    Add files via upload

---
+++ data_sending.js
@@ -0,0 +1,17 @@
+function mostrarInput(){
+    const idEspacio = document.getElementById("espacio1")
+    const button = document.getElementById('boton');
+    function changeContent() {
+        div.innerHTML = '<p>New HTML content!</p>';
+      }
+
+    button.addEventListener('click', changeContent);
+}
+
+function enviarDatos(){
+
+}
+
+function llenarDatos(){
+
+}
